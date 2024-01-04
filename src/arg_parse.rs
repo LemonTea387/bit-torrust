ac4ea39d50f62e6c9fa49a24076e3445ca586768
LemonTea387 <JunJieLow08@gmail.com>
2024-01-04 23:33:39 +0800

    Simple cli parser separating the logic out

---
+++ src/arg_parse.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub action: Action,
+}
+
+#[derive(Subcommand)]
+pub enum Action {
+    Decode { bencode: String },
+    Info { file: PathBuf },
+}
